b7834723d62f914a3d973f979e4451fd3ae16e63
Elizandro Moura <j.elizandromoura@gmail.com>
2021-04-30 23:17:37 -0300

    Pagina loginAdm pronto
    pagina de loginAdm finalizado

---
+++ login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="estilo.css">
+
+    <title>Thais Xavier Oficial</title>
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="shortcut icon" href="imagens2/heart_crown_princess_emoji_emo_icon_131636.ico" type="image/x-icon">
+
+    
+
+  </head>
+  <body class="corpo">
+    <header class="cabecalho">
+      
+
+      <nav class="menu">
+        <ul>
+          <li>
+            <a href="index.html " class="menu__link">HOME</a>
+          </li>
+         
+        </ul>
+      </nav>
+    </header>
+    
+    <main class="principaladm">
+     
+      <section>  
+
+        
+            <div class="login">
+                <h1 class="titulologin">Login Adm</h1>
+                <input type="email" name="email" id="emailadm" placeholder="Email"> <br> <br>
+                <input type="password" name="senha" id="senhaadm" placeholder="Senha"> <br> <br>
+                <input type="submit" value="logar">
+            </div>
+
+
+       
+
+      </section><!--fim conteudo-->
+    </main>
+    <footer class="rodape">
+      <p class="rodape__texto">Todos os direitos reservados.</p>
+     </footer>
+
+  </body>
+</html>
